7071813468bcc73d7f540ea7f45a57de3bf5b6f5
Dustin Hunter <102769956+LacunaStrategies@users.noreply.github.com>
2022-04-20 05:20:55 -0700

    Updated H1 and Paragraph

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,9 @@
 const contractAddress = "0x2058AA9450A1e7eafD19D20b60e4a804AaE19d83"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to the JHart Watchers NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = 'The JHart Watchers NFT Project is the first series to be released by JHart, a young digital artist!  Each piece is hand-drawn and unique; not digitally randomized or generated.  This limited set from the Watchers series is an opportunity for JHart to share their art with the greater world in the exciting new NFT space.  If you are not familiar with purchasing NFTs, helpful links have been included below:';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
